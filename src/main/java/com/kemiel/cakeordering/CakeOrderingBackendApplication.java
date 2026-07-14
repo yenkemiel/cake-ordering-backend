d9543aae864d582cd625ec77c1b56d9cecd8a2c7
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-07-14 14:14:52 +0800

    chore: 補上 Spring Initializr 專案骨架遺漏檔案

---
+++ src/main/java/com/kemiel/cakeordering/CakeOrderingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kemiel.cakeordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeOrderingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeOrderingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kemiel/cakeordering/CakeOrderingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kemiel.cakeordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeOrderingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
