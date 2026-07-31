224ba56cdf762178a6cdf996cc462eda8c8a998e
Kemiel Yen <102275234+yenkemiel@users.noreply.github.com>
2026-07-31 09:42:30 +0800

    feat(order): 新增 OrderRepository

---
+++ src/main/java/com/kemiel/cakeordering/order/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.kemiel.cakeordering.order.repository;
+
+import com.kemiel.cakeordering.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * 訂單 Repository
+ */
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByOrderNoAndPhone(String orderNo, String phone);
+}
